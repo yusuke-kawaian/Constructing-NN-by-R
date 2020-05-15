a23534f576488b124200906ae496e381294a4bab
Kawaian <63406643+yusuke-kawaian@users.noreply.github.com>
2020-05-15 16:43:14 +0900

    Add files via upload

--- pred_P_nnet_NN.R
+++ pred_P_nnet_NN.R
@@ -4,25 +4,26 @@
 library(caret)
 library(doParallel)
 
+cl_n <- detectCores()
+cl <- makePSOCKcluster(cl_n)
+registerDoParallel(cl)
+
 #install.packages('nnet') 
+#omstall.paclages('sigmoid')
 library(nnet)
+library(sigmoid)
 
 #install.packages("NeuralNetTools")
 library(NeuralNetTools)
 
-setwd("C:/Users/kawak/Desktop/pred_P")
+setwd("C:/Users/kawak/Desktop/pred_P/R")
 
 #inout data
 analysis_data <- read.table("/Users/kawak/Desktop/pred_P/normed_dataset.txt", skip = 0, header=TRUE, sep=" ");
 
 dataset <- analysis_data[,c(3,4,5,6,7,8,9)]
-p_total <- analysis_data[,c(10)]
+p_total <- analysis_data[,c(10)]*100
 
-
-#normalization
-norm <- function(x){
-  return((x-min(x)) / (max(x)-min(x)))
-}
 
 #normed_data <- cbind(as.data.frame(lapply(dataset, norm)),p_total)
 normed_data <- cbind(dataset,p_total)
@@ -40,7 +41,7 @@ test_data <- test[,c(1,2,3,4,5,6,7)]
 test_labels <- test[,c(8)]
 
 
-nn <- nnet(train_data, train_labels, size = 100, rang = 0.5, decay = 0, maxit = 1000)
+nn <- nnet(train_data, train_labels, linout = TRUE, size = 100, rang = 0.5, decay = 0, maxit = 1000)
 
 summary(nn)
 #source("http://hosho.ees.hokudai.ac.jp/~kubo/log/2007/img07/plot.nn.txt")
@@ -49,6 +50,19 @@ summary(nn)
 plotnet(nn)
 
 pred_test <- predict(nn,test_data)
+
+shape <- function(x){
+  if (x < 0) {
+    return(0)
+  } else if (x > 100) {
+    return(100)
+  } else {
+    return(x)
+  }
+}
+
+
+pred_test <- as.data.frame(apply(pred_test, 1, shape))
 result <- cbind(test_labels,pred_test)
 
 write.table(result, file = "clipboard", sep="\t")
@@ -58,6 +72,7 @@ pred_data <- pred_dataset[,c(3,4,5,6,7,8,9)]
 #pred_data <- as.data.frame(lapply(pred_data, norm))
 #pred_data <- cbind(mass = pred_data[,c(1)], valent = pred_dataset[,c(4)], pred_data[,c(2,3,4,5,6)])
 pred_value <- predict(nn,pred_data)
-out_data <- cbind(pred_dataset,pred_value)
-write.table(out_data, "/Users/kawak/Desktop/pred_P/predict_nnet.txt", quote = F, col.names = T, row.names = F)
+pred_value <- as.data.frame(apply(pred_value, 1, shape))
+out_data <- cbind(pred_dataset[,c(1,4,5)],pred_value)
+write.table(out_data, "/Users/kawak/Desktop/pred_P/R/predict_nnet.txt", quote = F, col.names = T, row.names = F)
 write.table(out_data, file = "clipboard", sep="\t")
