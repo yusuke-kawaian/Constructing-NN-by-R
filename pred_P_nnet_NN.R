3fe31c50d420a044f6baa31b03b0f13f52373a14
Kawaian <63406643+yusuke-kawaian@users.noreply.github.com>
2020-05-19 16:03:47 +0900

    Add files via upload

--- pred_P_nnet_NN.R
+++ pred_P_nnet_NN.R
@@ -1,6 +1,6 @@
 #prediction by NN of nnet
 
-#library(neuralnet)
+
 library(caret)
 library(doParallel)
 
@@ -8,25 +8,29 @@ cl_n <- detectCores()
 cl <- makePSOCKcluster(cl_n)
 registerDoParallel(cl)
 
-#install.packages('nnet') 
-#omstall.paclages('sigmoid')
 library(nnet)
 library(sigmoid)
-
-#install.packages("NeuralNetTools")
 library(NeuralNetTools)
 
 setwd("C:/Users/kawak/Desktop/pred_P/R")
 
 #inout data
-analysis_data <- read.table("/Users/kawak/Desktop/pred_P/normed_dataset.txt", skip = 0, header=TRUE, sep=" ");
+analysis_data <- read.table("/Users/kawak/Desktop/pred_P/dataset.txt", skip = 0, header=TRUE, sep=" ");
 
 dataset <- analysis_data[,c(3,4,5,6,7,8,9)]
 p_total <- analysis_data[,c(10)]*100
 
 
-#normed_data <- cbind(as.data.frame(lapply(dataset, norm)),p_total)
-normed_data <- cbind(dataset,p_total)
+normalization <- read.table("/Users/kawak/Desktop/pred_P/normalization.txt", skip = 0, header=TRUE, sep=" ");
+normalization[,c(1,2)] <- NULL
+
+norm <- function(x){
+  return(abs(x-normalization[1,])/(normalization[2,]-normalization[1,]))
+}
+  
+normed_data <- apply(dataset, 1, norm)
+normed_data <- do.call("rbind", normed_data)
+normed_data <- cbind(normed_data,p_total)
 
 #product dataset
 smp.size <- floor(0.75*nrow(normed_data))
@@ -41,15 +45,11 @@ test_data <- test[,c(1,2,3,4,5,6,7)]
 test_labels <- test[,c(8)]
 
 
-nn <- nnet(train_data, train_labels, linout = TRUE, size = 100, rang = 0.5, decay = 0, maxit = 1000)
+nn <- nnet(train_data, train_labels, linout = TRUE, size = 100, rang = 0.5, decay = 0, maxit = 2000)
 
 summary(nn)
-#source("http://hosho.ees.hokudai.ac.jp/~kubo/log/2007/img07/plot.nn.txt")
-#plot.nn(nn)
-
 plotnet(nn)
 
-pred_test <- predict(nn,test_data)
 
 shape <- function(x){
   if (x < 0) {
@@ -61,11 +61,11 @@ shape <- function(x){
   }
 }
 
-
+pred_test <- predict(nn,test_data)
 pred_test <- as.data.frame(apply(pred_test, 1, shape))
 result <- cbind(test_labels,pred_test)
-
 write.table(result, file = "clipboard", sep="\t")
+
 
 pred_dataset <- read.table("/Users/kawak/Desktop/pred_P/predict_dataset.txt", skip = 0, header=TRUE, sep=" ");
 pred_data <- pred_dataset[,c(3,4,5,6,7,8,9)]
